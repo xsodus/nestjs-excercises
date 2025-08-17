c11aec075955ecf69ded52449549f7141dc0da69
xsodus <apocalyps26@gmail.com>
2025-08-17 20:22:37 +0700

    feat(exercise-1): add LoggerService using MessageFormatter

--- exercise-1/src/app.module.ts
+++ exercise-1/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { MessageFormatter } from './message-formatter';
+import { LoggerService } from './logger.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, MessageFormatter],
+  providers: [AppService, MessageFormatter, LoggerService],
 })
 export class AppModule {}
---
+++ exercise-1/src/logger.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { MessageFormatter } from './message-formatter';
+
+@Injectable()
+export class LoggerService {
+  constructor(private readonly formatter: MessageFormatter) {}
+
+  /**
+   * Formats the message using MessageFormatter and logs it to console.
+   * Returns the formatted value for convenience/testing.
+   */
+  log(message: string): string {
+    const formatted = this.formatter.fomat(message);
+    // eslint-disable-next-line no-console
+    console.log(formatted);
+    return formatted;
+  }
+}
